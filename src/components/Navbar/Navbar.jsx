85681d4aac67d30fe3126613e251c692ee9c8057
Asif Azimi <asifazimi620@gmail.com>
2022-07-04 14:20:33 +0430

    Navbar added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.scss";
+import { links } from "../../data";
+import { HiMenuAlt4, HiX } from "react-icons/hi";
+import { motion } from "framer-motion";
+// Import Images
+import { images } from "../../constants";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  // states
+  const [toggle, setToggle] = useState(false);
+
+  return (
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <img src={images.logo} alt="logo" />
+      </div>
+      <ul className="app__navbar-links">
+        {links.map((link) => {
+          const { id, url, text } = link;
+          return (
+            <li key={id} className="app__flex p-text">
+              <div />
+              <a href={url}>{text}</a>
+            </li>
+          );
+        })}
+      </ul>
+
+      <div className="app__navbar-menu">
+        <HiMenuAlt4 onClick={() => setToggle(true)} />
+
+        {toggle && (
+          <motion.div
+            whileInView={{ x: [300, 0] }}
+            transition={{ duration: 0.85, ease: "easeOut" }}
+          >
+            <HiX onClick={() => setToggle(false)} />
+            <ul>
+              {links.map((link) => {
+                const { id, url, text } = link;
+                return (
+                  <li key={id} className="">
+                    <a href={url} onClick={() => setToggle(false)}>
+                      {text}
+                    </a>
+                  </li>
+                );
+              })}
+            </ul>
+          </motion.div>
+        )}
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
---
+++ src/data.js
@@ -0,0 +1,27 @@
+export const links = [
+  {
+    id: 0,
+    url: "/",
+    text: "home",
+  },
+  {
+    id: 1,
+    url: "/about",
+    text: "about",
+  },
+  {
+    id: 2,
+    url: "/work",
+    text: "work",
+  },
+  {
+    id: 3,
+    url: "/skills",
+    text: "skills",
+  },
+  {
+    id: 4,
+    url: "/contact",
+    text: "contact",
+  },
+];
